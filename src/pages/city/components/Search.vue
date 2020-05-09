c24a49ffd93a5304716d39b12ad3df9b65f4153c
qiguoqi <2280220550@qq.com>
2020-05-09 20:59:21 +0800

    add input

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入拼音名或拼音" class="search-input">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CitySearch"
+  }
+</script>
+
+<style lang="stylus" scoped>
+@import "~@/assets/styles/varibles.styl"
+.search
+  height .72rem
+  background $bgColor
+  padding 0 .1rem
+  .search-input
+    box-sizing border-box
+    width 100%
+    height .62rem
+    padding 0 .1rem
+    line-height .62rem
+    text-align center
+    border-radius .06rem
+    color #666
+</style>
