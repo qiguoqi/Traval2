f105430d5110302bac0414171747df226502a987
qiguoqi <2280220550@qq.com>
2020-05-09 19:23:19 +0800

    axios

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// import store from './store'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
+// require('swiper/dist/css/swiper.css')
+import 'swiper/dist/css/swiper.css'
+
+
+Vue.config.productionTip = false
+fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
