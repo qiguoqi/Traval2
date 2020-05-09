ada2dad6e048cfcbfe7843f36863d362af9cf19e
qiguoqi <2280220550@qq.com>
2020-05-09 23:31:30 +0800

    add city

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CityAlphabet"
+  }
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+.list
+  display flex
+  flex-direction column
+  justify-content center
+  position absolute
+  top 1.58rem
+  right 0
+  bottom 0
+  width .4rem
+  .item 
+    text-align center
+    line-height .44rem
+    color $bgColor
+</style>
