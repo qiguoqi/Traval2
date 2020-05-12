b1db3b0b794ca94bc985fbc009ef458e278afabc
qiguoqi <2280220550@qq.com>
2020-05-12 20:38:39 +0800

    keep-alive

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons';
 import HomeRecommend from './components/Recommend';
 import HomeWeekend from './components/Weekend';
 import axios from 'axios';
+import { mapState } from 'vuex'
 export default {
   name: "Home",
   components: {
@@ -29,12 +30,13 @@ export default {
       swiperList: [],
       iconList: [],
       recommendList: [],
-      weekendList: []
+      weekendList: [],
+      lastCity: ''
     }
   },
   methods: {
     getHomeInfo() {
-      axios.get("/api/index.json")
+      axios.get("/api/index.json?city=" + this.city)
         .then(res => {
           this.getHomeInfoSucc(res)
         })
@@ -50,7 +52,17 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  computed: {
+    ...mapState(['city'])
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
